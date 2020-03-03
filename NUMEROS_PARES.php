8c40d96e5a6fa8ec5d638d852162a36be6ba5b25
Villegas78 <60683440+Villegas78@users.noreply.github.com>
2020-03-02 23:31:46 -0600

    Add files via upload

---
+++ NUMEROS_PARES.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NUMEROS PARES</title>
+</head>
+<body bgcolor="violet">
+
+</body>
+</html>
+<center><h1>Estos son los 10 primeros numeros pares</h1></center>
+
+<?php
+	$par[0]="2";
+	$par[1]="4";
+	$par[2]="6";
+	$par[3]="8";
+	$par[4]="10";
+	$par[5]="12";
+	$par[6]="14";
+	$par[7]="16";
+	$par[8]="18";
+	$par[9]="20";
+
+	echo $par[0], ",",$par[1], ",",$par[2], ",",$par[3], ",",$par[4], ",",$par[5], ",",$par[6], ",",$par[7], ",",$par[8], ",",$par[9];
+
+	  ?>
